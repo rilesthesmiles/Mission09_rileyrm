aa81b3701a4450de8e958db154d758628402f415
Riley Marshall <rileyrm@byu.edu>
2023-03-13 16:53:43 -0600

    I added comments and the shopping cart icon in the top right corner

---
+++ OnlineBookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
